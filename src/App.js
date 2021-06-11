6fa19eca4705f97dc5467cd67917259ae4576c8e
David <davidamedina619@gmail.com>
2021-06-10 22:07:28 -0400

    app missing unit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import react from 'react'
 import './App.css';
-import Main from Main.js
+import Game from 'Game.js'
+import Main from 'Main.js'
+import Player from 'Player.js'
+import Test from 'Test.js'
 
 
 function App() {
   return (
     <div className="App">
      <Main />
+     <Game />
+     <Test />
+     <Player />
     </div>
   );
 }
